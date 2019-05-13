226f2b708c3ad16949a08ffd7707c6227f34e9a9
SteveFWagner <sfwagner1011@gmail.com>
2019-05-13 10:12:34 -0600

    added comments

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 //ListItem displays the title recieved from props
 const ListItem = (props) => {
-    if(props.title.link){
+    if(props.title.link){ //checking if the link exists on the props.title object
         const {link,title} = props.title
-        const augmentedLink = `https://stackoverflow.com${link}`
+        const augmentedLink = `https://stackoverflow.com${link}` //adding the stackoverflow base url to the link scraped
         return (
             <div className='list-item'>
-                <h2><a href={augmentedLink}>{title}</a></h2>
+                <h2><a href={augmentedLink} target='_blank' rel="noopener noreferrer">{title}</a></h2>
             </div>
         );
-    }else{
+    }else{ //if there is no link, only display the title
         return (
             <div className='list-item'>
                 <h2>{props.title.title}</h2>
